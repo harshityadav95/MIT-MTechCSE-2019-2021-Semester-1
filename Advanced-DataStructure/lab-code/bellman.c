2216eedb379f52e9db4c5f2a2bd6b3a181a5f18a
harshityadav95 <harshityadav95@gmail.com>
2019-10-03 15:00:24 +0530

    HPCS+adsa lab final

--- Advanced-DataStructure/lab-code/bellman.c
+++ Advanced-DataStructure/lab-code/bellman.c
@@ -1,48 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void Bellmanford(int graph[20][20],int V, int E,int edge[20][20]);
-
-void main(){
-int i,j,k=0,edge[20][20],graph[20][20],V;
-printf("enter the no. of vertices:\n");
-scanf("%d",&V);
-printf("enter the graph in matrix form:\n");
-for(i=0;i<V;i++)
+void Bellmanford(int graph[10][10],int V, int E,int edge[10][10]);
+void main()
 {
-for(j=0;j<V;j++)
-{
-scanf("%d",&graph[i][j]);
-if(graph[i][j]!=0)
-edge[k][0]=i, edge[k++][1]=j;
-}
-}
-Bellmanford(graph,V,k,edge);
-}
-
-void Bellmanford(int graph[20][20],int V, int E,int edge[20][20])
-{
-int u,v,i,k;
-int dist[20],s;
-for(i=0;i<V;i++)
-{
-dist[i]=1000;
-}
-printf("enter the destination:\n");
-scanf("%d",&s);
-dist[s-1]=0;
-for(i=0;i<V-1;i++)
-{
-	for(k=0;k<E;k++)
-	 {
-		u=edge[k][0], v=edge[k][1];
-		if(dist[u] + graph[u][v] < dist[v])
-		dist[v] = dist[u] + graph[u][v];
+	int i,j,k=0,edge[10][10],graph[10][10],V;
+	printf("No.of nodes :");
+	scanf("%d",&V);
+	printf("Graph weights:\n");
+	for(i=0;i<V;i++)
+	{
+		for(j=0;j<V;j++)
+		{
+			scanf("%d",&graph[i][j]);
+			if(graph[i][j]!=0)
+			edge[k][0]=i,
+			edge[k++][1]=j;
+		}
 	}
+	Bellmanford(graph,V,k,edge);
 }
-for(i=0;i<V;i++)
+void Bellmanford(int graph[10][10],int V, int E,int edge[10][10])
 {
-printf("vertex = %d -> cost = %d",i+1,dist[i]);
-printf("\n");
-}
+	int u,v,i,k;
+	int dist[10],s;
+	for(i=0;i<V;i++)
+	{
+		dist[i]=1000;
+	}
+	printf("Source is 1 enter the destination:\n");
+	scanf("%d",&s);
+	dist[s-1]=0;
+	for(i=0;i<V-1;i++)
+	{
+		for(k=0;k<E;k++)
+		{
+			u=edge[k][0], v=edge[k][1];
+			if(dist[u] + graph[u][v] < dist[v])
+			{	
+				dist[v] = dist[u] + graph[u][v];
+			}
+		}
+	}
+	for(i=0;i<V;i++)
+	{
+		printf("Distance from vertex = %d to destination %d = %d",i+1,s,dist[i]);
+		printf("\n");
+	}
 }
--- Advanced-DataStructure/lab-code/flloyd.c
+++ Advanced-DataStructure/lab-code/flloyd.c
@@ -1,49 +1,12 @@
 #include<stdio.h> 
+#include<stdlib.h>
 #define V 4 
 #define INF 999 
-
-void floydWarshall (int graph[][V]) 
-{ 
-	int dist[V][V], i, j, k; 
-
-	for (i = 0; i < V; i++) 
-		for (j = 0; j < V; j++) 
-			dist[i][j] = graph[i][j]; 
-
-	for (k = 0; k < V; k++) 
-	{ 
-		
-		for (i = 0; i < V; i++) 
-		{ 
-			for (j = 0; j < V; j++) 
-			{ 
-				
-				if (dist[i][k] + dist[k][j] < dist[i][j]) 
-					dist[i][j] = dist[i][k] + dist[k][j]; 
-			} 
-		} 
-	} 
-
-	printf ("The following matrix shows the shortest distances"
-			" between every pair of vertices \n"); 
-	for (int i = 0; i < V; i++) 
-	{ 
-		for (int j = 0; j < V; j++) 
-		{ 
-			if (dist[i][j] == 999) 
-				printf("%7s", "INF"); 
-			else
-				printf ("%7d", dist[i][j]); 
-		} 
-		printf("\n"); 
-	} 
-} 
-
- 
+void floydWarshall (int graph[][V]); 
 int main() 
 { 
 	int i,j,graph[V][V];
-	printf("enter the weighted graph:\n");
+	printf("Enter Value :\n");
 	for(i=0;i<V;i++)
 	{
 		for(j=0;j<V;j++)
@@ -54,4 +17,41 @@ int main()
 	
 	floydWarshall(graph); 
 	return 0; 
+}
+void floydWarshall (int graph[][V]) 
+{ 
+	int dist[V][V], i, j, k; 
+	for (i = 0; i < V; i++) 
+	{
+		for (j = 0; j < V; j++)dist[i][j] = graph[i][j]; 
+	}		
+	for (k = 0; k < V; k++) 
+	{		
+		for (i = 0; i < V; i++) 
+		{ 
+			for (j = 0; j < V; j++) 
+			{ 
+				if (dist[i][k] + dist[k][j] < dist[i][j]) dist[i][j] = dist[i][k] + dist[k][j]; 
+			} 
+		} 
+	} 
+	printf ("shortest distances \n"); 
+	for (int i = 0; i < V; i++) 
+	{ 
+		for (int j = 0; j < V; j++) 
+		{ 
+			if (dist[i][j] == 999) 
+			{
+				printf("%s", "INF"); 
+			}
+				
+			else
+			{
+				printf ("%d      ", dist[i][j]);
+			}
+				 
+		} 
+		printf("\n"); 
+	} 
 } 
+ 
