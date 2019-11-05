58f4623664343393402b22e0b999ba81584ccd05
harshityadav95 <harshityadav95@gmail.com>
2019-11-05 08:53:53 +0530

    save

---
+++ Advanced-DataStructure/lab-code/t1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int v;
+	int a[20][20],k=0,edge[20][20];
+	scanf("%d",&v);
+	for (int i = 0; i < v; ++i)
+	{
+		for (int j = 0; j < v; ++j)
+		{
+			scanf("%d",&a[i][j])
+			if (a[i][j]!=0)
+			{
+				
+			}
+		}
+	}
+
+	return 0;
+}
